d2005f15374e9844ac36e112c49b799fc163b4b7
Kevin Silverstein <kats@umn.edu>
2022-02-21 19:36:07 -0600

    fixed input and output directory specs in script and added input file.

--- myscript2.sh
+++ myscript2.sh
@@ -6,11 +6,12 @@
 DIR=$HOME/Practice
 COLS=2-4,14
 OUT=$DIR/test_example
+IN=$DIR/g2f_2015_hybrid_data_raw.csv
 
 echo -n 'Starting script: ' ; date
 cd $DIR
 if [ ! -d $OUT ]; then mkdir $OUT; fi
-cut -d , -f $COLS g2f_2015_data/a._2015_hybrid_phenotypic_data/g2f_2015_hybrid_data_raw.csv > $OUT/00_planting_dates.txt
+cut -d , -f $COLS $IN > $OUT/00_planting_dates.txt
 sort --field-separator=, $OUT/00_planting_dates.txt > $OUT/01_planting_dates_sorted.txt
 uniq -c $OUT/01_planting_dates_sorted.txt > $OUT/02_city_date_counts.txt
 sort -rn $OUT/02_city_date_counts.txt > $OUT/03_city_date_counts_sorted.txt
