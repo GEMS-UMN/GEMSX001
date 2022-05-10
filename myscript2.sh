47872161a47bcb76f4c127e6395b461d5ef765a8
Kevin Silverstein <kats@umn.edu>
2022-05-10 10:34:04 -0500

    added newline spacing to make the code more readable.

--- myscript2.sh
+++ myscript2.sh
@@ -11,9 +11,11 @@ IN=$DIR/g2f_2015_hybrid_data_raw.csv
 echo -n 'Starting script: ' ; date
 cd $DIR
 if [ ! -d $OUT ]; then mkdir $OUT; fi
+
 cut -d , -f $COLS $IN > $OUT/00_planting_dates.txt
 sort --field-separator=, $OUT/00_planting_dates.txt > $OUT/01_planting_dates_sorted.txt
 uniq -c $OUT/01_planting_dates_sorted.txt > $OUT/02_city_date_counts.txt
 sort -rn $OUT/02_city_date_counts.txt > $OUT/03_city_date_counts_sorted.txt
+
 echo -n 'Finishing script: ' ; date
 
